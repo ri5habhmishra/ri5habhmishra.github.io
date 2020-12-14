dbea6b222ca8b89110aceffd0fadbfa45f9be1bf
rishabh <rishabhm1974@gmail.com>
2020-12-14 14:28:50 +0530

    added page scroll indicator

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,19 @@ $(function() {
     }
   })
 })
+
+
+// scrollIndicator starts
+
+window.addEventListener('scroll', moveScrollIndicator);
+
+const scrollIndicatorElt = document.getElementById('scrollIndicator');
+const maxHeight = window.document.body.scrollHeight - window.innerHeight;
+
+function moveScrollIndicator(e) {
+  const percentage = ((window.scrollY) / maxHeight) * 100;
+
+  scrollIndicatorElt.style.width = percentage + '%';
+}
+
+// scrollIndicator ends
